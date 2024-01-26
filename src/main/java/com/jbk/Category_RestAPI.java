5f663ebee3803012062b94cdce7f2e53c7b54d3c
“siddhesh-bhavsargit config --global user.email “siddheshbhavsar00@gmail.com <siddheshbhavsarsb.11@gmail.com>
2024-01-26 14:43:51 +0530

    first commit

--- src/main/java/com/jbk/Category_RestAPI.java
+++ src/main/java/com/jbk/Category_RestAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EMartRestApiApplication {
+public class Category_RestAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EMartRestApiApplication.class, args);
+		SpringApplication.run(Category_RestAPI.class, args);
 	}
 
 }
